46277469f1ac30a0198199983c5f369aa2957f2d
David Chen <darkbaby123@gmail.com>
2016-10-18 22:36:18 +0800

    implement algorithim and test

---
+++ test/insertion-sort-test.js
@@ -0,0 +1,9 @@
+const insertionSort = require('../lib/insertion-sort')
+
+describe('insertionSort', () => {
+  it('[6, 5, 3, 1, 8, 7, 2, 4]', () => {
+    expect(
+      insertionSort([6, 5, 3, 1, 8, 7, 2, 4])
+    ).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+  })
+})
